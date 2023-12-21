6c7a1caf7f3cc1b2998c0084d33818a45fa53f9a
GaurvPatil <gaurvpatil17@gmail.com>
2023-12-21 18:56:26 +0530

    prisma , graphQL and apollo client setup completed with basic user Query

---
+++ graphQL/schema.js
@@ -0,0 +1,62 @@
+// export const typeDefs = `#graphql
+
+// type User {
+//   id: ID!
+//   name: String
+//   email: String!
+//   password: String!
+
+//  posts: [Post]!
+
+// }
+
+// type Post {
+//   id: ID!
+//   slug: String
+//   title: String
+//   body: String
+// }
+
+// type Query {
+//   Users: [User]!
+
+// }
+
+// type Mutation {
+//   createUser(name: String, email: String, password: String, posts: [PostInput]): User
+// }
+
+// input PostInput {
+
+//     slug: String
+//     title: String
+//     body: String
+
+// }
+
+// `;
+
+export const typeDefs = `#graphql
+
+type User {
+  id: ID!
+  name: String
+  email: String!
+  password: String!
+
+}
+
+
+type Query {
+  Users: [User]!
+  
+}
+
+type Mutation {
+  createUser(name: String, email: String, password: String, ): User
+}
+
+
+
+
+`;
---
+++ index.js
@@ -0,0 +1,41 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./graphQL/schema.js";
+import prisma from "./prisma/index.js";
+
+const resolvers = {
+  Query: {
+    Users: async () => {
+      return await prisma.user.findMany();
+    },
+  },
+
+  Mutation: {
+    createUser: async (_parent, args) => {
+      console.log(args);
+     
+
+      const User = await prisma.user.create({
+        data: {
+          name: args.name,
+          email: args.email,
+          password: args.password,
+          // posts:[...args.posts],
+        },
+      });
+
+      return User;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("server ready at port : ", 4000);
